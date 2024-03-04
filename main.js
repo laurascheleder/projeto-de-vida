b4d0b3ef3bd512f34b7ae38c3a8798e3c1eefdfa
laurascheleder <117988728+laurascheleder@users.noreply.github.com>
2024-03-04 09:29:57 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+const botoes = document.querySelectorAll(«.botao»);
+console.log(botoes)
+for (let i=0; i < botões.length; i++) { console.log(i); }
 
+         for(let i=0;i <botoes.length;i++){
+             { botoes[j].classList.remove(«ativo»);
+      }
+         botoes[i].classList.add(«ativo»);
+    }
+}
